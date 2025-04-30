bf01d225fd384434a708b9a3e6f41da416585ce6
abirahmmed12 <abirahmedchowdhury12@gmail.com>
2025-04-30 22:21:57 +0600

    Initial commit

---
+++ class.py
@@ -0,0 +1,12 @@
+class Print_name():
+    name=''
+    age=0
+student1=Print_name()
+student1.name='abir '
+print(student1.name)
+
+num=[45,4,34,34,23,45,45]
+number =4
+for i in num:
+    if i==4:
+        print(i,"in the index"[i])
